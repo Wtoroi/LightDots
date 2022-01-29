c944e701a9eaa774799a00ea34f9157931ff9bd5
Wtoroi <98623625+Wtoroi@users.noreply.github.com>
2022-01-29 13:13:42 +0200

    LightDots

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace Light_Dots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picture.Location = new System.Drawing.Point(0, 0);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(805, 482);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(678, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(678, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 482);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.picture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dots";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Drawing2D;
+
+namespace Light_Dots
+{
+    public partial class Form1 : Form
+    {
+        Thread myThread;
+        int PosX = Cursor.Position.X;
+        int PosY = Cursor.Position.Y;
+        double d = 0;
+        int StarsNum = 4500;
+        int radius;
+        int radiusK = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for(int i = 0; i < StarsNum; i++)
+                dots.Add(new LDot(rnd.Next(0, picture.Width), rnd.Next(0, picture.Height)));
+            for (int i = 0; i < StarsNum / 100; i++)
+                Bigdots.Add(new LDot(rnd.Next(0, picture.Width), rnd.Next(0, picture.Height)));
+            radius = picture.Height / radiusK;
+
+            myThread = new Thread(Run);
+            myThread.Start();
+        }
+
+
+        List<LDot> dots = new List<LDot>();
+        List<LDot> Bigdots = new List<LDot>();
+
+        public void Run()
+        {
+            reDrow();
+
+            while (true)
+            {
+                textBox1.Invoke((MethodInvoker)(() => textBox1.Text = PointToClient(Cursor.Position).X.ToString()));
+                textBox2.Invoke((MethodInvoker)(() => textBox2.Text = PointToClient(Cursor.Position).Y.ToString()));
+                PosX = Convert.ToInt32(textBox1.Text);
+                PosY = Convert.ToInt32(textBox2.Text);
+
+                Random rnd = new Random();
+                int R = 0;
+                int G = 0;
+                double k = 0;
+                double kk = 0;
+
+                foreach (LDot s in dots)
+                {
+                    d = Math.Pow(PosX - s.X, 2) + Math.Pow(PosY - s.Y, 2);
+                    d = Math.Sqrt(d);
+
+                    R = rnd.Next(0, 255);
+                    G = rnd.Next(0, 255);
+                    Color с = Color.FromArgb(255, R, G, 255);
+                    kk = 1 - Math.Sin(d / radius);
+
+                    if (d < radius)
+                        s.ChengColor(Color.FromArgb(с.A, (int)(с.R * kk), (int)(с.G * kk), (int)(с.B * kk)));
+                    else
+                        s.ChengColor(picture.BackColor);
+                }
+
+                foreach (LDot s in Bigdots)
+                {
+                    d = Math.Pow(PosX - s.X, 2) + Math.Pow(PosY - s.Y, 2);
+                    d = Math.Sqrt(d);
+                    kk = 1 - Math.Sin(d / radius);
+                    k = Convert.ToDouble(rnd.Next(70, 100)) / 100;
+
+                    if (d < radius)
+                        s.ChengColor(Color.FromArgb((int)(s.C.A * k), (int)(s.C.R * k * kk), (int)(s.C.G * k * kk), (int)(s.C.B * k * kk)));
+                    else
+                        s.ChengColor(picture.BackColor);
+                }
+
+                reDrow();
+            }
+        }
+
+
+        public void reDrow()
+        {
+            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(picture.BackColor);
+            try
+            {
+                foreach (LDot s in dots)
+                {
+                    g.FillRectangle(s.Brush, s.X, s.Y, 1, 1);
+                }
+                foreach (LDot s in Bigdots)
+                {
+                    g.FillEllipse(s.Brush, s.X, s.Y, 3, 3);
+                }
+            }
+            catch { }
+            picture.Invoke((MethodInvoker)(() => picture.Image = bmp));
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myThread.Abort();
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            myThread.Abort();
+            radius = picture.Height / radiusK;
+
+            Random rnd = new Random();
+            dots.Clear();
+            Bigdots.Clear();
+            for (int i = 0; i < StarsNum; i++)
+                dots.Add(new LDot(rnd.Next(0, picture.Width), rnd.Next(0, picture.Height)));
+            for (int i = 0; i < StarsNum / 100; i++)
+                Bigdots.Add(new LDot(rnd.Next(0, picture.Width), rnd.Next(0, picture.Height)));
+            
+            myThread = new Thread(Run);
+            myThread.Start();
+        }
+    }
+}
+
+public class LDot
+{
+    public int X, Y;
+    public Color C = Color.White;
+    public SolidBrush Brush = new SolidBrush(Color.Blue);
+
+    public LDot(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void ChengColor(Color c)
+    {
+        Brush = new SolidBrush(c);
+    }
+}
